bfe180c8aa4eeb2f2dda5e388795ca55daf05a9a
Moesaeah King <mkc42@mail.missouri.edu>
2017-12-09 23:33:49 -0600

    Adding Additional Files

---
+++ dist/assets/tests.js
@@ -0,0 +1,350 @@
+'use strict';
+
+define('super-rentals/tests/acceptance/list-rentals-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  var EmberObject = Ember.Object;
+  var run = Ember.run;
+
+
+  var rental = EmberObject.create({
+    image: 'fake.png',
+    title: 'test-title',
+    owner: 'test-owner',
+    category: 'test-type',
+    city: 'test-city',
+    bedrooms: 3
+  });
+
+  (0, _emberQunit.moduleForComponent)('rental-listing', 'Integration | Component | rental listing', {
+    integration: true
+  });
+
+  (0, _emberQunit.test)('should display rental details', function (assert) {
+    this.set('rentalObj', rental);
+    this.render(Ember.HTMLBars.template({
+      "id": "L1FoWnqL",
+      "block": "{\"symbols\":[],\"statements\":[[1,[25,\"rental-listing\",null,[[\"rental\"],[[20,[\"rentalObj\"]]]]],false]],\"hasEval\":false}",
+      "meta": {}
+    }));
+    assert.equal(this.$('.listing h3').text(), 'test-title', 'Title: test-title');
+    assert.equal(this.$('.listing .owner').text().trim(), 'Owner: test-owner', 'Owner: test-owner');
+  });
+
+  (0, _emberQunit.test)('should toggle wide class on click', function (assert) {
+    this.set('rentalObj', rental);
+    this.render(Ember.HTMLBars.template({
+      "id": "L1FoWnqL",
+      "block": "{\"symbols\":[],\"statements\":[[1,[25,\"rental-listing\",null,[[\"rental\"],[[20,[\"rentalObj\"]]]]],false]],\"hasEval\":false}",
+      "meta": {}
+    }));
+    assert.equal(this.$('.image.wide').length, 0, 'initially rendered small');
+    run(function () {
+      return document.querySelector('.image').click();
+    });
+    assert.equal(this.$('.image.wide').length, 1, 'rendered wide after click');
+    run(function () {
+      return document.querySelector('.image').click();
+    });
+    assert.equal(this.$('.image.wide').length, 0, 'rendered small after second click');
+  });
+});
+define('super-rentals/tests/app.lint-test', [], function () {
+  'use strict';
+
+  QUnit.module('ESLint | app');
+
+  QUnit.test('adapters/application.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'adapters/application.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'app.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('components/rental-listing.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'components/rental-listing.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('resolver.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'resolver.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('router.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'router.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('routes/about.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/about.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('routes/contact.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/contact.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('routes/index.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/index.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('routes/rentals.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/rentals.js should pass ESLint\n\n');
+  });
+});
+define('super-rentals/tests/helpers/destroy-app', ['exports'], function (exports) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = destroyApp;
+  var run = Ember.run;
+  function destroyApp(application) {
+    run(application, 'destroy');
+  }
+});
+define('super-rentals/tests/helpers/module-for-acceptance', ['exports', 'qunit', 'super-rentals/tests/helpers/start-app', 'super-rentals/tests/helpers/destroy-app'], function (exports, _qunit, _startApp, _destroyApp) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  exports.default = function (name) {
+    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+    (0, _qunit.module)(name, {
+      beforeEach: function beforeEach() {
+        this.application = (0, _startApp.default)();
+
+        if (options.beforeEach) {
+          return options.beforeEach.apply(this, arguments);
+        }
+      },
+      afterEach: function afterEach() {
+        var _this = this;
+
+        var afterEach = options.afterEach && options.afterEach.apply(this, arguments);
+        return resolve(afterEach).then(function () {
+          return (0, _destroyApp.default)(_this.application);
+        });
+      }
+    });
+  };
+
+  var resolve = Ember.RSVP.resolve;
+});
+define('super-rentals/tests/helpers/start-app', ['exports', 'super-rentals/app', 'super-rentals/config/environment'], function (exports, _app, _environment) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = startApp;
+  var merge = Ember.merge;
+  var run = Ember.run;
+  function startApp(attrs) {
+    var attributes = merge({}, _environment.default.APP);
+    attributes = merge(attributes, attrs); // use defaults, but you can override;
+
+    return run(function () {
+      var application = _app.default.create(attributes);
+      application.setupForTesting();
+      application.injectTestHelpers();
+      return application;
+    });
+  }
+});
+define('super-rentals/tests/integration/components/rental-listing-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  var EmberObject = Ember.Object;
+  var run = Ember.run;
+
+
+  var rental = EmberObject.create({
+    image: 'fake.png',
+    title: 'test-title',
+    owner: 'test-owner',
+    category: 'test-type',
+    city: 'test-city',
+    bedrooms: 3
+  });
+
+  (0, _emberQunit.moduleForComponent)('rental-listing', 'Integration | Component | rental listing', {
+    integration: true
+  });
+
+  (0, _emberQunit.test)('should display rental details', function (assert) {
+    this.set('rentalObj', rental);
+    this.render(Ember.HTMLBars.template({
+      "id": "L1FoWnqL",
+      "block": "{\"symbols\":[],\"statements\":[[1,[25,\"rental-listing\",null,[[\"rental\"],[[20,[\"rentalObj\"]]]]],false]],\"hasEval\":false}",
+      "meta": {}
+    }));
+    assert.equal(this.$('.listing h3').text(), 'test-title', 'Title: test-title');
+    assert.equal(this.$('.listing .owner').text().trim(), 'Owner: test-owner', 'Owner: test-owner');
+  });
+
+  (0, _emberQunit.test)('should toggle wide class on click', function (assert) {
+    this.set('rentalObj', rental);
+    this.render(Ember.HTMLBars.template({
+      "id": "L1FoWnqL",
+      "block": "{\"symbols\":[],\"statements\":[[1,[25,\"rental-listing\",null,[[\"rental\"],[[20,[\"rentalObj\"]]]]],false]],\"hasEval\":false}",
+      "meta": {}
+    }));
+    assert.equal(this.$('.image.wide').length, 0, 'initially rendered small');
+    run(function () {
+      return document.querySelector('.image').click();
+    });
+    assert.equal(this.$('.image.wide').length, 1, 'rendered wide after click');
+    run(function () {
+      return document.querySelector('.image').click();
+    });
+    assert.equal(this.$('.image.wide').length, 0, 'rendered small after second click');
+  });
+});
+define('super-rentals/tests/test-helper', ['super-rentals/app', '@ember/test-helpers', 'ember-qunit'], function (_app, _testHelpers, _emberQunit) {
+  'use strict';
+
+  (0, _testHelpers.setApplication)(_app.default.create({ autoboot: false }));
+
+  (0, _emberQunit.start)();
+});
+define('super-rentals/tests/tests.lint-test', [], function () {
+  'use strict';
+
+  QUnit.module('ESLint | tests');
+
+  QUnit.test('acceptance/list-rentals-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'acceptance/list-rentals-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('helpers/destroy-app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/destroy-app.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('helpers/module-for-acceptance.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/module-for-acceptance.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('helpers/start-app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/start-app.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('integration/components/rental-listing-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'integration/components/rental-listing-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('test-helper.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'test-helper.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/adapters/application-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/adapters/application-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/routes/about-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/routes/about-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/routes/contact-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/routes/contact-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/routes/index-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/routes/index-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/routes/rentals-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/routes/rentals-test.js should pass ESLint\n\n');
+  });
+});
+define('super-rentals/tests/unit/adapters/application-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleFor)('adapter:application', 'Unit | Adapter | application', {
+    // Specify the other units that are required for this test.
+    // needs: ['serializer:foo']
+  });
+
+  // Replace this with your real tests.
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var adapter = this.subject();
+    assert.ok(adapter);
+  });
+});
+define('super-rentals/tests/unit/routes/about-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleFor)('route:about', 'Unit | Route | about', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('super-rentals/tests/unit/routes/contact-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleFor)('route:contact', 'Unit | Route | contact', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('super-rentals/tests/unit/routes/index-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleFor)('route:index', 'Unit | Route | index', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('super-rentals/tests/unit/routes/rentals-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleFor)('route:rentals', 'Unit | Route | rentals', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+require('super-rentals/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+//# sourceMappingURL=tests.map
